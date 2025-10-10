995feaade1c225de8b476d955e6a2ce4cb27af78
Guilherme Lins <guilhermelins978@gmail.com>
2025-10-10 14:33:15 -0300

    CRIANDO COMPONENTES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostHeading } from "@/components/PostHeading";
 import { PostList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
@@ -12,21 +12,23 @@ export default async function HomePage() {
       <Header />
 
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image
-            className="w-full object-cover object-center h-full group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
-            width={1200}
-            height={720}
-            alt="Título"
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{ href: "/post/dawdawd" }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Alt da image",
+            priority: true,
+          }}
+        />
         <div className="flex flex-col gap-4 sm:justify-center">
-          <time className="text-slate-600 text-sm/tight" dateTime="2025-04-20">20/04/2025 10:00</time>
-          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
-            <Link href="#">Lorem ipsum dolor sit amet consecteturblan</Link>
-          </h1>
+          <time className="text-slate-600 text-sm/tight" dateTime="2025-04-20">
+            20/04/2025 10:00
+          </time>
+          <PostHeading as="h1" url="#">
+            Lorem ipsum, dolor sit amet consectetur
+          </PostHeading>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est
             natus necessitatibus modi pariatur quidem dolorem veritatis, nulla
