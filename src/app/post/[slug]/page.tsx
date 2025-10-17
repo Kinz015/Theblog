97e40cd77503b29eb58c1b63f74c3d1bb8c2b702
Guilherme Lins <guilhermelins978@gmail.com>
2025-10-17 14:11:19 -0300

    Revert "teste"
    This reverts commit 8714f092aa6ea6d66673a3e32a4b6f14d2376537.

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,6 @@
 import SinglePost from "@/components/SinglePost";
 import { SpinLoader } from "@/components/SpinLoader";
-import {
-  findAllPostBySlugCached,
-  findAllPublicPostsCached,
-} from "@/lib/post/queries";
+import { findAllPostBySlugCached } from "@/lib/post/queries";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
@@ -21,20 +18,6 @@ export async function generateMetadata({
     title: post.title,
     description: post.excerpt,
   };
-}
-
-export async function generateStaticParams() {
-  const posts = await findAllPublicPostsCached();
-
-  const params = posts.map((post) => {
-    return {
-      slug: post.slug,
-    };
-  });
-
-  console.log(params)
-
-  return params;
 }
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
