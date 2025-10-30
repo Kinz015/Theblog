811c9cdd5e1b923977f97c71afd2e9787b54a7f0
Guilherme Lins <guilhermelins978@gmail.com>
2025-10-30 12:15:37 -0300

    criei novas rotas

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,10 +1,12 @@
-'use server';
+"use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function revalidateExampleAction(formData: FormData) {
-  const path = formData.get('path') || '';
-  console.log('Estou em uma server action', path);
+  const path = formData.get("path") || "";
+  console.log("Estou em uma server action", path);
 
-  revalidatePath(`${path}`)
+  // revalidatePath(`${path}`)
+  revalidateTag("posts");// home
+  revalidateTag("posts-rotina-matinal-de-pessoas-altamente-eficazes");// home
 }
