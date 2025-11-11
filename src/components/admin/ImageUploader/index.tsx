e3bf43999509f7ac168820d8c64da2c082026053
Guilherme Lins <guilhermelins978@gmail.com>
2025-11-10 21:06:08 -0300

    criei o uploadImageAction

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,16 +1,47 @@
 "use client";
 
+import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
+import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/post/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useTransition } from "react";
+import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
 
-    fileInputRef.current.click()
+    fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+      toast.error(`Image muito grande. Máx.: ${readableMaxSize}KB.`);
+
+      fileInput.value = "";
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // TODO: Criar a action para upload de arquivo
+    startTransition(async () => {
+      const result = await uploadImageAction()
+    })
+
+    fileInput.value = "";
   }
 
   return (
@@ -21,6 +52,7 @@ export function ImageUploader() {
       </Button>
 
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className="hidden"
         name="file"
